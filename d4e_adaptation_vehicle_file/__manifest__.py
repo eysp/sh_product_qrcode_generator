e9d007deb292ceee5c7120833fa2252915862b6e
rihabjl <rihabjl>
2021-06-09 08:29:07 +0100

    add d4e_create_order_adaptation + d4e_adaptation_vehicle_file

---
+++ d4e_adaptation_vehicle_file/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Vehicle file adaptation",
+    'description': """
+        Adaptation of the vehicle file in the Fleet module
+    """,
+    'author': "D4E",
+    'website': "https://www.d4e.cool",
+    'category': 'Tools',
+    'version': '14.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['fleet'],
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/fleet_views.xml',
+        'views/fleet_insurance_view.xml',
+    ],
+
+}
