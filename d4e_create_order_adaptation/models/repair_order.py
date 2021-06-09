a3f697e94fb21a758fd243783161ed2c07f2b028
rihabjl <rihabjl>
2021-06-09 09:44:34 +0100

    amèlioration du code

--- d4e_create_order_adaptation/models/repair_order.py
+++ d4e_create_order_adaptation/models/repair_order.py
@@ -8,6 +8,18 @@ class Repair(models.Model):
 
     vehicle_id = fields.Many2one('fleet.vehicle', 'Vehicle', tracking=True, domain="['|', ('driver_id', '=', False), ('driver_id', '=', partner_id)]")
     mileage = fields.Float(string='Mileage')
+    product_id = fields.Many2one(
+        'product.product', string='Product to Repair',
+        domain="[('type', 'in', ['product', 'consu']), '|', ('company_id', '=', company_id), ('company_id', '=', False)]",
+        readonly=True, required=False, states={'draft': [('readonly', False)]}, check_company=True)
+    product_uom = fields.Many2one(
+        'uom.uom', 'Product Unit of Measure',
+        readonly=True, required=False, states={'draft': [('readonly', False)]}, domain="[('category_id', '=', product_uom_category_id)]")
+    location_id = fields.Many2one(
+        'stock.location', 'Location',
+        index=True, readonly=True, required=False, check_company=True,
+        help="This is the location where the product to repair is located.",
+        states={'draft': [('readonly', False)], 'confirmed': [('readonly', True)]})
 
     @api.onchange('vehicle_id')
     def on_change_client(self):
