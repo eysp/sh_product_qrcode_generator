50d439f95f0fd1338eada71b92a61f4285f9956c
rihabjl <rihabjl>
2021-06-25 17:00:03 +0100

    développements de phase 3

--- d4e_default_data/__manifest__.py
+++ d4e_default_data/__manifest__.py
@@ -10,14 +10,16 @@
     'version': '14.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['account', 'contacts', 'l10n_ch'],
+    'depends': ['account', 'base', 'contacts', 'l10n_ch'],
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'data/account_tax_data.xml',
         'data/account_data.xml',
         'views/account.xml',
         'views/res_partner.xml',
         'views/template.xml',
+        'views/res_bank.xml',
+        'views/postal_number.xml',
     ],
 }
--- d4e_default_data/models/__init__.py
+++ d4e_default_data/models/__init__.py
@@ -2,3 +2,5 @@
 
 from . import account_model
 from . import res_partner
+from . import res_bank
+from . import postal_number
