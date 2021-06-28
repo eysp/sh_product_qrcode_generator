c30c8d65534ff1e0707313b53c2aa6af214ccf2d
rihabjl <rihabjl>
2021-06-28 16:26:31 +0100

    update traduction

--- d4e_pack_accounting/models/postal_number.py
+++ d4e_pack_accounting/models/postal_number.py
@@ -6,24 +6,14 @@ class PostalNumber(models.Model):
     _description = 'Postal Number'
     _name = 'postal.number'
 
-    REC_ART = fields.Char()
-    ONRP = fields.Char()
-    BFSNR = fields.Char()
-    PLZ_TYP = fields.Char()
-    POSTLEITZAHL = fields.Char()
-    PLZ_ZZ = fields.Char()
-    GPLZ = fields.Char()
-    ORTBEZ18 = fields.Char()
-    ORTBEZ27 = fields.Char()
-    KANTON = fields.Char()
-    SPRACHCODE = fields.Char()
-    SPRACHCODE_ABW = fields.Char()
-    BRIEFZ_DURCH = fields.Char()
-    GILT_AB_DAT = fields.Char()
-    PLZ_BRIEFZUST = fields.Char()
-    PLZ_COFF = fields.Char()
-    Geo_Shape = fields.Text()
-    Geokoordinaten_1 = fields.Text()
-    Geokoordinaten_2 = fields.Text()
+    onrp = fields.Char(string='Identifiant')
+    bfsnr = fields.Char(string='N° OFS')
+    plz_typ = fields.Char(string='Type de NPA')
+    kanton = fields.Char(string='Canton')
+    gplz = fields.Char(string='NPA')
+    ortbez18 = fields.Char(string='Localité')
+    sprachcode = fields.Selection([('1', 'Allemand'), ('2', 'Français'), ('3', 'Italien')], string="Langue", default='1')
+
+
 
 
