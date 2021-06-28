bc0f7db3499d0254f13062f45ad6ef97f2cbd1ac
rihabjl <rihabjl>
2021-06-28 14:49:44 +0100

    add d4e_pack_accounting

---
+++ d4e_pack_accounting/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "D4e Pack Accounting",
+    'description': """
+        D4e Pack Accounting
+    """,
+    'author': "D4E",
+    'website': "https://www.d4e.cool",
+    'category': 'Tools',
+    'version': '14.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account', 'base', 'contacts', 'l10n_ch', 'hr'],
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account.xml',
+        'views/res_partner.xml',
+        'views/template.xml',
+        'views/res_bank.xml',
+        'views/postal_number.xml',
+    ],
+}
