16b770e3208bb193866a15ca29663a8be290a673
rihabjl <rihabjl>
2021-06-29 17:03:49 +0100

    update phase2+phase3

--- d4e_pack_accounting/models/res_bank.py
+++ d4e_pack_accounting/models/res_bank.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
+import json
+
 from odoo.osv import expression
 from odoo import api, fields, models, _
+import requests
 
 
 class ResBank(models.Model):
     _inherit = "res.bank"
 
     clearing = fields.Char(string="Clearing")
+    swissbank_id = fields.Char()
 
     def name_get(self):
         res = super(ResBank, self).name_get()
@@ -25,3 +29,24 @@ class ResBank(models.Model):
             if operator in expression.NEGATIVE_TERM_OPERATORS:
                 domain = ['&'] + domain
         return self._search(domain + args, limit=limit, access_rights_uid=name_get_uid)
+
+    @api.model
+    def create_bank_with_api(self):
+        url = "https://api.six-group.com/api/epcd/bankmaster/v2/public"
+        reponse = requests.get(url).json()
+        for rec in reponse['entries']:
+            swissbank_id = str(rec.get('group') or '') + str(rec.get('iid') or '') + str(rec.get('branchId') or '') + str(rec.get('sicIid') or '') + str(rec.get('headOffice') or '')
+            bank_vals = {
+                'name': rec.get('bankOrInstitutionName') or '',
+                'phone': rec.get('phone') or '',
+                'zip': rec.get('zipCode') or '',
+                'street': rec.get('domicileAddress') or '',
+                'city': rec.get('place') or '',
+                'bic': rec.get('bic') or '',
+                'swissbank_id': swissbank_id,
+            }
+            bank = self.env["res.bank"].search([('swissbank_id', '=', bank_vals['swissbank_id'])])
+            if bank:
+                bank.write(bank_vals)
+            else:
+                self.env["res.bank"].create(bank_vals)
--- d4e_pack_accounting/models/res_partner.py
+++ d4e_pack_accounting/models/res_partner.py
@@ -6,19 +6,27 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    customer_no = fields.Integer(string="Customer no.", default=0)
+    def compute_customer_no(self):
+        num = self.env['res.partner'].search([])
+        maxi = num.mapped('customer_no')
+        if not maxi:
+            return 1
+        return max(maxi) + 1
+
+    customer_no = fields.Integer(string="Customer no.", default=compute_customer_no)
+    phone_2 = fields.Char(string="Phone 2")
 
     @api.onchange('zip')
     def on_change_zip(self):
-        post = self.env['postal.number'].search([('POSTLEITZAHL', '=', self.zip)])
+        post = self.env['postal.number'].search([('gplz', '=', self.zip)])
         if post:
-            self.city = post[0].ORTBEZ27
+            self.city = post[0].ortbez18
 
     @api.onchange('city')
     def on_change_city(self):
-        post = self.env['postal.number'].search([('ORTBEZ27', '=', self.city)])
+        post = self.env['postal.number'].search([('ortbez18', '=', self.city)])
         if post:
-            self.zip = post[0].POSTLEITZAHL
+            self.zip = post[0].gplz
 
     @api.model
     def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
