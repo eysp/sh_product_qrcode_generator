c12d489c6cedea428ee837ff976280dd3bc1bb8f
yosra ammar <yosra.ammar@campion-tech.com>
2021-07-29 16:18:41 +0100

    fiche véhicule

--- d4e_adaptation_vehicle_file/models/fleet.py
+++ d4e_adaptation_vehicle_file/models/fleet.py
@@ -6,6 +6,13 @@ from odoo import api, fields, models
 class FleetVehicle(models.Model):
     _inherit = 'fleet.vehicle'
 
+
+    def _compute_count_repair(self):
+        repair = self.env['repair.order']
+        for record in self:
+            record.repair_count = repair.search_count([('vehicle_id', '=', record.id)])
+
+
     driving_license = fields.Binary(string='Driving license', readonly=False, help='Upload the file')
     last_expertise = fields.Date('Last Expertise Date', required=False, default=fields.Date.today, tracking=True)
     weight = fields.Float(string='Weight')
@@ -15,6 +22,19 @@ class FleetVehicle(models.Model):
     key_identification = fields.Char('Key Identification')
     policy_number = fields.Char('Policy N°')
     insurance_id = fields.Many2one('fleet.vehicle.log.insurance', 'Insurance', tracking=True)
+    repair_count = fields.Integer(compute="_compute_count_repair", string='Repairs Count')
+
+    def return_repair_to_open(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Repairs',
+            'view_mode': 'tree',
+            'res_model': 'repair.order',
+            'domain': [('vehicle_id', '=', self.id)],
+        }
+
+
 
 
 
